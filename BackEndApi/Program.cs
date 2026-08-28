ad9431203824f8623fdd3a36b73f677887afc0ab
NaveenBoopathy <naveen.b@ecgroup-intl.com>
2026-08-28 17:01:40 +0530

    Docker Changes

--- BackEndApi/Program.cs
+++ BackEndApi/Program.cs
@@ -25,11 +25,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
